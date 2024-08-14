396369b5deb60edb68bcd82324c9eb7947bdd813
izabelly-code <izabelly.mucholowskiribeiro@gmail.com>
2024-08-13 22:09:26 -0300

    try use oauth2

--- src/main/java/com/quth/demo/Controller/LoginController.java
+++ src/main/java/com/quth/demo/Controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.quth.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to our website!");
+        return "index"; // Thymeleaf template name (greeting.html)
+    }
 }
