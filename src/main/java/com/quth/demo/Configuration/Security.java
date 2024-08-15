d0b03a74f7a701f081a076fbc58cbf250a62266d
izabelly-code <izabelly.mucholowskiribeiro@gmail.com>
2024-08-14 22:11:02 -0300

    try use oauth2

---
+++ src/main/java/com/quth/demo/Configuration/Security.java
@@ -0,0 +1,9 @@
+package com.quth.demo.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+}
